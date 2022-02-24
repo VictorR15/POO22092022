afeba3f0f2561451cba820578571910e975155be
VictorR15 <victor1203rmmtz@gmail.com>
2022-02-24 12:17:08 -0600

    Proyecto conceptos basicos

---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,19 @@
+
+package javaapplication4;
+
+
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        // TODO code application logic here
+    
+        
+        System.out.println("Hola de nuevo");
+    }
+    
+    
+}
