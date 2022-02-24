83926364fa56a6181f4d5dc5103e76bdd8d23fa8
VictorR15 <victor1203rmmtz@gmail.com>
2022-02-24 12:40:23 -0600

    Iniciamos el archivo foco

---
+++ JavaApplication4/src/ico/fes/Foco.java
@@ -0,0 +1,44 @@
+
+package ico.fes;
+
+
+public class Foco {
+    
+    //Atributos
+    
+    private String color;
+    private String tipo;
+    private int tamano;
+    
+    //Constructores 
+
+    public Foco() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+    
+    
+    
+}
